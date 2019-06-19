595044968971f99bfc24f25d43301b79da1d2223
JaredDyreson <jared.dyreson@gmail.com>
2019-06-18 23:43:19 -0700

    Allows for the use of internet outside of installation

--- engineer_man_follow.sh
+++ engineer_man_follow.sh
@@ -2,7 +2,7 @@
 
 # Very helpful video --> https://www.youtube.com/watch?v=UzESH4KK8qs&t=2294s
 
-#timedatectl set-ntp true
+timedatectl set-ntp true
 
 # Partitioning the drives
 
@@ -59,12 +59,12 @@ password=`dialog --stdout --passwordbox "Enter admin password" 0 0`
 password2=`dialog --stdout --passwordbox "Enter admin password again" 0 0`
 [[ -z "$password2" ]] && (echo "password seems to be empty";passwd)
 clear
-#[[ "$password" == "$password2" ]] || ( echo "Passwords did not match"; exit 1; )
+[[ "$password" != "$password2" ]] && ( echo "Passwords did not match"; exit 1; )
 
 #echo "root:$password" | chpasswd --root /mnt
 
 # Working with 
-pacman -Sy --noconfirm grub efibootmgr
+pacman -Sy --noconfirm grub efibootmgr ipw2200-fw
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
