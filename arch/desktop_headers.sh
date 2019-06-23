9dbb84aa69293ffff962673abf0baf6d9e383873
JaredDyreson <jared.dyreson@gmail.com>
2019-06-23 13:32:47 -0700

    Restructured how the program works so it should work

--- arch/desktop_headers.sh
+++ arch/desktop_headers.sh
@@ -1,26 +1,18 @@
 #!/usr/bin/env bash
 
-# Header files for desktop_installer_main.sh 
-
-## Functions that are used for creating a user space 
-
 function make_root() {
 	[[ "$(whoami)" != "root" ]] && (echo "Run as root!";exit)
 	echo "$1 ALL=(ALL) ALL"  | tee -a /etc/sudoers
 }
 
 function password_manager(){
-	# REFACTOR 
-	[[ -z "$1" ]] && return
-	password_one=""
-	password_two="different"
-	while [[ "$password_one" != "$password_two" || -z "$password_one" || -z "$password_two" ]]; do
-		password_one=$(dialog --stdout --passwordbox "Enter admin password" 0 0) || exit 1
+	passone=""
+	passtwo="d"
+	while [[ -z "$passone" || "$passone" != "$passtwo" ]]; do
+		passone=$(dialog --stdout --passwordbox "Enter admin password" 0 0) || exit 1
 		clear
-		[[ -z "$password_one" ]] && (echo "Password cannot be nothing")
-		password_two=$(dialog --stdout --passwordbox "Enter admin password again" 0 0) || exit 1
+		passtwo=$(dialog --stdout --passwordbox "Enter admin password again" 0 0) || exit 1
 		clear
-		[[ "$password" != "$password2" ]] && ( echo "Passwords did not match";)	
 	done
 	echo "$1:$password_one" | chpasswd "$1"
 	echo "root:$password_one" | chpasswd root
@@ -33,10 +25,9 @@ function create_user() {
 	[[ -z "$1" || `check_user "$1"` ]] && exit
 	useradd -m -g users -G wheel,storage,power -s /bin/zsh "$1"
 	password_manager "$1"
-	mkdir -p ~/{Applications,archives,Downloads,Documents,Music,Pictures/Wallpapers,Projects,Video}
-	cd ~/Projects
+	mkdir -p /home/"$user"/{Applications,archives,Downloads,Documents,Music,Pictures/Wallpapers,Projects,Video}
 	cat /tmp/dotfiles/manifest_lists/repo_manifest | while read line; do
-		git clone "$line"
+		git clone "$line" /home/"$user"/Projects/"$line"
 	done
 }
 
@@ -63,71 +54,65 @@ function terminal_configuration() {
 	curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh  >> omzinstaller
 	chmod +x ./omzinstaller
 	echo "Y" | ./omzinstaller
-	git clone https://github.com/AlexisBRENON/oh-my-zsh-reminder ~/.oh-my-zsh/custom/plugins/reminder
-	cp -ar /tmp/dotfiles/shell/zshrc ~/.zshrc
-	echo "$pass" | sudo -S pacman -Sy --noconfirm vim cmake
-	echo "$pass" | yay -Sy --noconfirm vundle
-	cp -ar /tmp/dotfiles/shell/vimrc ~/.vimrc
-	#vim -u +PluginInstall +qall
-	#/usr/bin/python ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
-	echo "$pass" | sudo -S pacman -Sy --noconfirm rxvt-unicode xorg-xrdb ttf-dejavu powerline powerline-fonts ranger
+	git clone https://github.com/AlexisBRENON/oh-my-zsh-reminder /home/"$user"/.oh-my-zsh/custom/plugins/reminder
+	cp -ar /tmp/dotfiles/shell/zshrc /home/"$user"/.zshrc
+	pacman -Sy --noconfirm vim cmake
+	sudo -u builduser bash -c "yay -Sy --noconfirm vundle"
+	cp -ar /tmp/dotfiles/shell/vimrc /home/"$user"/.vimrc
+	vim +silent +PluginInstall +qall
+	/usr/bin/python /home/"$user"/.vim/bundle/YouCompleteMe/install.py --clang-completer
+	pacman -Sy --noconfirm rxvt-unicode xorg-xrdb ttf-dejavu powerline powerline-fonts ranger
 }
 
 function desktop_manager(){
-	echo "$pass" | sudo -S pacman -Sy --noconfirm xorg-server lightdm lightdm-gtk-greeter cinnamon noto-fonts
-	echo "$pass" | yay -Sy --noconfirm lightdm-slick-greeter
+	pacman -Sy --noconfirm xorg-server lightdm lightdm-gtk-greeter cinnamon noto-fonts
+	sudo -u builduser bash -c "yay -Sy --noconfirm lightdm-slick-greeter"
 	sudo sed -i 's/#greeter-session=.*/greeter-session=lightdm-slick-greeter/' /etc/lightdm/lightdm.conf
 	systemctl enable lightdm.service
 }
 
 
 function theme_manager() {
-	cd /tmp
-	echo "$pass" | yay -Sy --noconfirm mint-x-icons mint-y-icons mint-themes
-	git clone https://github.com/daniruiz/flat-remix
-	git clone https://github.com/daniruiz/flat-remix-gtk
-	mkdir -p ~/{.icons,.themes}
-	cp -r flat-remix/Flat-Remix* ~/.icons/ && cp -r flat-remix-gtk/Flat-Remix-GTK* ~/.themes/
-	rm -rf flat*
+	themes=('mint-x-icons' 'mint-y-icons' 'mint-themes')
+	for theme in "$themes"; do
+		sudo -u builduser bash -c "yay -Sy --noconfirm "$theme""
+	done
+	mkdir -p /home/"$user"/{.icons,.themes}
+	git clone https://github.com/daniruiz/flat-remix /home/"$user"/.icons/
+	git clone https://github.com/daniruiz/flat-remix-gtk /home/"$user"/.themes/
 }
 
 function dot_file_installer() {
-	mkdir ~/.config/ranger 
-	cp -ar /tmp/dotfiles/ranger/* ~/.config/ranger/
-	cp -ar /tmp/dotfiles/terminal/Xresources ~/.Xresources
-	cp -ar /tmp/dotfiles/wallpaper/* ~/Pictures/Wallpapers/
+	mkdir /home/"$user"/.config/ranger 
+	cp -ar /tmp/dotfiles/ranger/* /home/"$user"/.config/ranger/
+	cp -ar /tmp/dotfiles/terminal/Xresources /home/"$user"/.Xresources
+	cp -ar /tmp/dotfiles/wallpaper/* /home/"$user"/Pictures/Wallpapers/
 	dconf load / < /tmp/dotfiles/desktop_env/arch_cinnamon_settings
 	git config --global user.name "Jared Dyreson"
 	git config --global user.email "jared.dyreson@gmail.com"
-	`cd ~ && git clone https://github.com/JaredDyreson/scripts.git`
+	`git clone https://github.com/JaredDyreson/scripts.git /home/"$user"/scripts`
 }
 
 function application_installer() {
-	echo "$pass" | sudo -S pacman -Sy --noconfirm vlc zenity firefox htop gnome-bluetooth file-roller 
-	echo "$pass" | yay -Sy --noconfirm spotify vmware-workstation ffmpeg-compat-57 shutter discord balena-etcher mintstick pix
-	echo "$pass" | sudo -S find /usr/share/applications/ -type f \(-name "*java*" -o -name "*avahi*" \) -exec rm -rf {} \;
+	pacman -Sy --noconfirm vlc zenity firefox htop gnome-bluetooth file-roller 
+	yay_applications=('spotify' 'vmware-workstation' 'ffmpeg-compat-57' 'shutter' 'discord' 'balena-etcher' 'mintstick' 'pix')
+	for application in "$yay_applications"; do
+		sudo -u builduser bash -c "yay -Sy --noconfirm "$application""
+	done
+	find /usr/share/applications/ -type f \(-name "*java*" -o -name "*avahi*" \) -exec rm -rf {} \;
 }
 
 
 function programming_environments(){
-	echo "$pass" | sudo -S pacman -Sy --noconfirm clang most jre-openjdk jdk-openjdk openjdk-doc python-pip
-
-	cd /tmp && git clone https://github.com/jeaye/stdman.git && cd stdman && ./configure && echo "$pass" | sudo -S make install && echo "$pass" | sudo -S mandb && cd .. && rm -rf stdman
-
+	pacman -Sy --noconfirm clang most jre-openjdk jdk-openjdk openjdk-doc python-pip
+	cd /tmp && git clone https://github.com/jeaye/stdman.git && cd stdman && ./configure && make install && mandb && cd .. && rm -rf stdman
 	cat /tmp/dotfiles/manifest_lists/python_packages | while read line; do
 		sudo pip install --upgrade "$line"
 		sudo pip3 install --upgrade "$line"
 	done
-
-}
-function clean_up(){
-	userdel builduser
-	rm -rf ~/pass
-	usermod -s /bin/zsh "$user"
-	reboot
 }
 
-### Set up logging ###
+# Set up logging 
 exec 1> >(tee "stdout.log")
 exec 2> >(tee "stderr.log")
 
@@ -139,5 +124,5 @@ dot_file_installer
 application_installer
 programming_environments
 terminal_configuration
-userdel builduser
+userdel -rf builduser
 reboot
