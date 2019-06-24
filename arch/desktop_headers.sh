72014359ed54bd91461ae980a33480dbe6e7ab08
JaredDyreson <jared.dyreson@gmail.com>
2019-06-24 01:25:55 -0700

    Fixed issue where git repos were being created by root instead of user

--- arch/desktop_headers.sh
+++ arch/desktop_headers.sh
@@ -28,7 +28,7 @@ function create_user() {
 	sudo -u "$1" bash -c "mkdir -p /home/"$1"/{Applications,archives,Downloads,Documents,Music,Pictures/Wallpapers,Projects,Video}"
 	cat /tmp/dotfiles/manifest_lists/repo_manifest | while read line; do
 		[[ $(echo "$line" | grep 'university') ]] && break
-		git clone "$line" /home/"$1"/Projects/"$(basename "$line" | sed 's/\.git//g')"
+		sudo -u "$1" bash -c "git clone "$line" /home/"$1"/Projects/"$(basename "$line" | sed 's/\.git//g')""
 	done
 }
 
@@ -39,8 +39,6 @@ function initial_configuration(){
 	sed -i 's/builduser.*//g;s/'$1'.*//g' /etc/sudoers
 	pacman -S --needed --noconfirm sudo git dialog python zsh
 	[[ `check_user builduser` ]] || (useradd builduser -m && passwd -d builduser && make_root builduser)
-	[[ -d /tmp/dotfiles ]] && rm -rf /tmp/dotfiles
-	git clone https://github.com/JaredDyreson/dotfiles.git /tmp/dotfiles
 	u="$1"
 	create_user "$u"
 	make_root "$u"
@@ -52,12 +50,12 @@ function initial_configuration(){
 ## Functions that are used for the installation of the actual desktop environment 
 
 function terminal_configuration() {
-	sudo -u "$user" bash -c "cp -ar /tmp/dotfiles/shell/zshrc /home/"$user"/.zshrc"
+	sudo -u "$user" bash -c "cp -ar /home/"$user"/Projects/dotfiles/shell/zshrc /home/"$user"/.zshrc"
 	curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | bash 
 	git clone https://github.com/AlexisBRENON/oh-my-zsh-reminder /home/"$user"/.oh-my-zsh/custom/plugins/reminder
 	pacman -Sy --noconfirm vim cmake
 	sudo -u builduser bash -c "yay -Sy --noconfirm vundle"
-	sudo -u "$user" bash -c "cp -ar /tmp/dotfiles/shell/vimrc /home/"$user"/.vimrc"
+	sudo -u "$user" bash -c "cp -ar /home/"$user"/Projects/dotfiles/shell/vimrc /home/"$user"/.vimrc"
 	vim +silent +PluginInstall +qall
 	/usr/bin/python /home/"$user"/.vim/bundle/YouCompleteMe/install.py --clang-completer
 	pacman -Sy --noconfirm rxvt-unicode xorg-xrdb ttf-dejavu powerline powerline-fonts ranger
