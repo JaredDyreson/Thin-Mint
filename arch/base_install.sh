b79f4b1f89660dd4de5eb7a18f62f46f93169e03
JaredDyreson <jared.dyreson@gmail.com>
2019-12-19 21:24:47 -0800

    converting the old code into the sed version

--- arch/base_install.sh
+++ arch/base_install.sh
@@ -16,23 +16,43 @@ TGTDEV="$(lsblk -dplnx size -o name,size | grep -Ev "boot|rpmb|loop" | tail -n 1
 # document what we're doing in-line with the actual commands
 # Note that a blank line (commented as "defualt" will send a empty
 # line terminated with a newline to take the fdisk default.
+#sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk "${TGTDEV}"
+  #o # clear the in memory partition table
+  #n # new partition
+  #p # primary partition
+  #1 # partition number 1
+    ## default - start at beginning of disk 
+  #+100M # 100 MB boot parttion
+  #n # new partition
+  #p # primary partition
+  #2 # partion number 2
+    ## default, start immediately after preceding partition
+    ## default, extend partition to end of disk
+  #a # make a partition bootable
+  #1 # bootable partition is partition 1 -- /dev/sda1
+  #p # print the in-memory partition table
+  #w # write the partition table
+  #q # and we're done
+#EOF
+
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk "${TGTDEV}"
   o # clear the in memory partition table
   n # new partition
   p # primary partition
-  1 # partition number 1
+    # partition number 1
     # default - start at beginning of disk 
   +100M # 100 MB boot parttion
-  n # new partition
-  p # primary partition
-  2 # partion number 2
-    # default, start immediately after preceding partition
-    # default, extend partition to end of disk
-  a # make a partition bootable
-  1 # bootable partition is partition 1 -- /dev/sda1
-  p # print the in-memory partition table
-  w # write the partition table
-  q # and we're done
+  a
+  t
+  ef
+  w
+  n
+  p
+
+
+
+  w
+  q
 EOF
 
 exit
