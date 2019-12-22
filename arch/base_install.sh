15ac0999d5c21dd0192eaa2860ac9ee37ffce1de
JaredDyreson <jared.dyreson@gmail.com>
2019-12-21 22:11:08 -0800

    moved the configuration file generation

--- arch/base_install.sh
+++ arch/base_install.sh
@@ -86,8 +86,8 @@ echo "127.0.0.1 localhost $hostname" > /etc/hosts
 
 ## Working with GRUB
 pacman -Sy --noconfirm grub efibootmgr ipw2200-fw lshw intel-ucode
-grub-install "$TGTDEV"
 grub-mkconfig -o /boot/grub/grub.cfg
+grub-install "$TGTDEV"
 
 ## internet persistance
 systemctl enable dhcpcd
