eb647ddb02034a6a8b3896277d9269c33e7d6cad
JaredDyreson <jared.dyreson@gmail.com>
2019-12-25 23:55:43 -0800

    changed my name in hardware_configuration

--- arch/hardware_configuration.sh
+++ arch/hardware_configuration.sh
@@ -14,13 +14,13 @@ pacman -Sy --noconfirm xf86-input-libinput libinput libinput-gestures xdotool
 
 # put the configuration we want directly from the dotfiles repo
 find /usr/share/X11/xorg.conf.d/ -type f -iname '*libinput.conf' | while read file; do
-	cat /home/"$user"/Projects/dotfiles/trackpad/libinput.conf > "$file"
+	cat /home/jared/Projects/dotfiles/trackpad/libinput.conf > "$file"
 done
-sudo gpasswd -a "$user" input
+sudo gpasswd -a jared input
 libinput-gestures-setup autostart
 echo "[+] Libinput gestures service started"
 sleep 10
-cp /home/"$user"/Projects/dotfiles/trackpad/libinput-gestures.conf /home/"$user"/.config/libinput-gestures.conf
+cp /home/jared/Projects/dotfiles/trackpad/libinput-gestures.conf /home/jared/.config/libinput-gestures.conf
 
 
 ## Battery
@@ -39,7 +39,7 @@ pacman -Sy --noconfirm acpi acpi_call-dkms
 modprobe acpi_call
 echo '_SB.PCI0.PEG0.PEGP._OFF' | sudo tee /proc/acpi/call
 echo acpi_call > /etc/modules-load.d/acpi_call.conf
-cp -ar /home/"$user"/Projects/dotfiles/graphics/dgpu-off.service /usr/lib/systemd/user/
+cp -ar /home/jared/Projects/dotfiles/graphics/dgpu-off.service /usr/lib/systemd/user/
 sudo systemctl enable /usr/lib/systemd/user/dgpu-off.service
 sed -i '/GRUB_CMDLINE_LINUX_DEFAULT/s/".*"/"modprobe.blacklist=nouveau i915.preliminary_hw_support=1 acpi_rev_override=5"/' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
